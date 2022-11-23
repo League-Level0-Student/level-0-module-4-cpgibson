d271b4d7e54e599d244d48cb7445e2484ac38c33
root <root@462ceea50067>
2022-11-23 02:23:35 +0000

    cs

--- src/_02_boolean/_4_red_square/RedSquare.java
+++ src/_02_boolean/_4_red_square/RedSquare.java
@@ -15,8 +15,8 @@ public class RedSquare {
 	
 	public static void main(String[] args) {
 		
-		boolean isRed = false;
-		boolean isSquare = true;
+		boolean isRed = true;
+		boolean isSquare = false;
 		
 		// The && shown here means that both booleans must be true for the 
 		// entire if statement to be true. It is referred to as the "and"
@@ -24,6 +24,15 @@ public class RedSquare {
 		if(isRed && isSquare) {
 			drawRedSquare();
 		}
+else if(!isRed && isSquare) {
+			drawBlueSquare();
+		}
+else if(isRed && !isSquare) {
+	drawRedTriangle();
+}
+else if(!isRed && !isSquare) {
+	drawBlueTriangle();
+}
 		else {
             JOptionPane.showMessageDialog(null, "No shape was drawn!");
         }
@@ -54,7 +63,12 @@ public class RedSquare {
 		// 7. Write an if statement using the static booleans at the top of 
 		//    the program. If drewRedSquare OR drewBlueSquare are true, then
 		//    tell the user they drew a square in a pop-up. Hint: ||
-		
+		if(drewRedSquare || drewBlueSquare) {
+			JOptionPane.showMessageDialog(null, "You drew a square.");
+		}
+		else if(drewRedTriangle || drewBlueTriangle) {
+			JOptionPane.showMessageDialog(null, "You drew a triangle.");
+		}
 		//    Notice these static booleans start off false, but update to true 
 		//    whenever you call their matching method.
 		
